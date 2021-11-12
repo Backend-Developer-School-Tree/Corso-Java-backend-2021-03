ad21e326df8a316ca9c7bcb9365a00e83b1d3dc1
Pietro Marchetto <pietromarchetto5@gmail.com>
2021-11-12 11:18:00 +0100

    Aggiunti metodi comparazione
    Aggiunti metodi compareTo, equals e hashCode a Prodotto

--- module_06/Cinema (esercizio guidato)/src/Prodotto.java
+++ module_06/Cinema (esercizio guidato)/src/Prodotto.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public abstract class Prodotto {
     private String name;
     private float prezzo;
@@ -35,5 +37,43 @@ public abstract class Prodotto {
 
     public boolean isDisponibile() {
         return quantita != 0;
+    }
+
+    /**
+     * Ritorna 0 se i due oggetti hanno lo stesso prezzo
+     * Ritorna 1 se prezzo > o.prezzo
+     * Ritorna -1 se prezzo < o.prezzo
+     * @param o
+     * @return
+     */
+    public int compareTo(Object o) {
+        Prodotto prodotto = (Prodotto) o;
+        return Float.compare(prezzo, prodotto.prezzo);
+    }
+
+    /**
+     * Due Prodotti sono uguali se hanno lo stesso nome.
+     * @param o
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        Prodotto prodotto = (Prodotto) o;
+        return Objects.equals(name, prodotto.name);
+    }
+
+    /**
+     * Hash code calcolato in base a nome, prezzo e quantità
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, prezzo, quantita);
     }
 }
